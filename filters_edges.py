973576cdeb6536b27e178839f1c3d035e471731f
eshwarne <northeasteditsofficial@gmail.com>
2019-06-20 21:50:30 +0530

    blurs

--- filters_edges.py
+++ filters_edges.py
@@ -1,1 +1,12 @@
 import cv2
+capture = cv2.VideoCapture(0)
+while True:
+    _, frame = capture.read()
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    res = cv2.bitwise_and(hsv,hsv)
+    median = cv2.medianBlur(res,15)
+    cv2.imshow('NEW',median)
+    if(cv2.waitKey(1) & 0xFF == ord('q') ):
+        break
+
+cv2.destroyAllWindows()
--- readUnreadable.py
+++ readUnreadable.py
@@ -1,15 +1,15 @@
 import cv2
 img  = cv2.imread('checks.jpg')
-scale_percent = 30 # percent of original size
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-# resize image
-resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-gray = cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
+# scale_percent = 30 # percent of original size
+# width = int(img.shape[1] * scale_percent / 100)
+# height = int(img.shape[0] * scale_percent / 100)
+# dim = (width, height)
+# # resize image
+# resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret,threshold = cv2.threshold(gray,11,255,cv2.THRESH_BINARY)
 guasThreshold = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,115,1)
-cv2.imshow('ORIGINAL',resized)
+cv2.imshow('ORIGINAL',img)
 cv2.imshow('thres',guasThreshold)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
